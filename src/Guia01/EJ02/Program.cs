5fff0e832653c9df9cf1d1d93107137f2a40a0e6
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-08 18:27:35 -0300

    Maqueta de lo que viene de TS Guia 01

---
+++ src/Guia01/EJ02/Program.cs
@@ -0,0 +1,41 @@
+﻿/* /program.cs 
+2. Crear una clase `Vehiculo` que tenga los atributos:
+públicos:
+    +`marca`,
+    +`modelo` 
+y un atributo privado:
+    + `patente`.
+- Crear una instancia y asignarle valores;
+notar que el atributo privado no está disponible para la asignación de valores.
+- Mostrar por pantalla los valores asignados.
+*/
+
+public class Program
+{
+    public static void Main()
+    {
+        // Creamos el primer vehiculo
+        Vehiculo v1 = new Vehiculo(
+            "Chevrolet",
+            "Spin",
+            "ZZZ000ZZ"
+        );
+
+        Console.WriteLine($"Marca: {v1.marca}");
+        Console.WriteLine($"Modelo: {v1.modelo}");
+        Console.WriteLine($"Patente: {v1.GetPatente()}");
+        // La patente es privada, por eso no se accede directamente.
+        // Se consulta mediante el método público GetPatente().
+        // Console.WriteLine(v1._patente); // Error: _patente es private
+
+        // Creamos el segundo Vehiculo
+        Vehiculo v2 = new Vehiculo(
+            "Ford",
+            "K",
+            "ZZZ999"
+        );
+        Console.WriteLine($"Marca: {v2.marca}");
+        Console.WriteLine($"Modelo: {v2.modelo}");
+        Console.WriteLine($"Patente: {v2.GetPatente()}");
+    }
+}
---
+++ src/Guia01/EJ02/Vehiculo.cs
@@ -0,0 +1,40 @@
+// Vehiculo.cs
+using System;
+
+public class Vehiculo
+{
+    // Atributos
+    public string marca;
+    public string modelo;
+    //private string _patente;
+    public string _patente;
+    // Constructor
+    public Vehiculo(string marca, string modelo, string patente)
+    {
+        this.marca = marca;
+        this.modelo = modelo;
+        this._patente = patente;
+    }
+    // Propiedades (get y set)
+    public string GetMarca()
+    {
+        return this.marca;
+    }
+    public void SetMarca(string marca)
+    {
+        this.marca = marca;
+    }
+    public string GetModelo()
+    {
+        return this.modelo;
+    }
+    public void SetModelo(string modelo)
+    {
+        this.modelo = modelo;
+    }
+    // Patente no tiene setters porque no queremos modificarlo desde afuera
+    public string GetPatente()
+    {
+        return this._patente;
+    }
+}
