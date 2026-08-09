25bcc25e3c63d57207a3bedc8d4c98b37d5d0a21
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-09 13:23:18 -0300

    Completamos el EJ01

--- src/Guia01/EJ01/Program.cs
+++ src/Guia01/EJ01/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Persona
+{
+    public string _nombre;
+    public string _apellido;
+
+    public Persona(string _nombre, string _apellido)
+    {
+        this._nombre = _nombre;
+        this._apellido = _apellido;
+    }
+
+    public string GetNombre()
+    {
+        return this._nombre;
+    }
+
+    public void SetNombre(string nombre)
+    {
+        this._nombre = nombre;
+    }
+
+    public string GetApellido()
+    {
+        return this._apellido;
+    }
+
+    public void SetApellido(string apellido)
+    {
+        this._apellido = apellido;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Persona myVariable = new Persona("Gastón", "Javier");
+
+        Console.WriteLine($"Nombre: {myVariable._nombre}");
+        Console.WriteLine($"Apellido: {myVariable._apellido}");
+    }
+}
