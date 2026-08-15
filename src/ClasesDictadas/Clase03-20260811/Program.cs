13f26c41523a1780f73269f14a6a5834a5acfb6a
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-14 21:37:58 -0300

    Agregamos solo el código de la clase 03

---
+++ src/ClasesDictadas/Clase03-20260811/Program.cs
@@ -0,0 +1,24 @@
+﻿// /Program.cs
+Persona unaPersona = new Persona("pepe", "loco");
+
+// tenemos que usarlo como un método (llamándolo)
+//unaPersona.SetName("    gastón     "); // Si hago    _uu no borra porque entiende que los espacios deberían estar.
+
+//unaPersona.LastName = "    ricchi"; // tenerlo como {get; set;}
+// Nos permite usarlo como un método entonces podemos guardar
+// Variables así
+
+string apellido = unaPersona.LastName;
+
+Console.WriteLine("Hola " + unaPersona.GetName() + " !");
+Console.WriteLine("Hola " + unaPersona.LastName + ", " + unaPersona.GetName() + " !");
+
+// Sting interpolletion se puede armar una cadena metiendo variables entre medio
+// Expresiones que generan un sting
+unaPersona = new Persona();
+
+unaPersona.SetName("Juancito");
+
+Console.WriteLine($"Hola {unaPersona.LastName}, {unaPersona.GetName()} !");
+
+Empleado unEmpleado = new Empleado();
