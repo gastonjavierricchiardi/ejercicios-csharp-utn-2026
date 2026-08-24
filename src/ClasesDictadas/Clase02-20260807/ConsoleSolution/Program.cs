41a59652a517e11f5be4bc64e68e8e3bacc97ba6
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-24 00:38:53 -0300

    Algumas modificaciones de las clases.

---
+++ src/ClasesDictadas/Clase02-20260807/ConsoleSolution/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("¡Bienvenidos a Programación II!");
+int dia = 7;
+
+Console.WriteLine("El día de hoy es: " + dia.ToString());
+
+string unaCadena = "Hola, estamos en Programación II";
+Console.WriteLine(unaCadena);
+
+Persona leonardo = new Persona();
+
+//Auto unAuto = new Auto();
+
+leonardo.apellido = "Pinkas";
+leonardo.nombre = "Leonardo";
+
+//Console.WriteLine(leonardo.apellido + ", " + leonardo.nombre);
+leonardo.Presentarse();
+
+
+Persona andres = new Persona();
+andres.apellido = "Chimuris";
+andres.nombre = "Andrés";
+
+
+andres.Saludar(leonardo);
+andres.SaludarA("Mauro");
+
+
+class Persona
+{
+    public string nombre;
+    public string apellido;
+
+    public void Presentarse()
+    {
+
+        Console.WriteLine(this.apellido + ", " + this.nombre);
+    }
+
+    public void Saludar(Persona aQuienSaludo)
+    {
+        Console.WriteLine("Hola " + aQuienSaludo.nombre + ", me llamo " + this.nombre);
+    }
+
+    public void SaludarA(string nombre)
+    {
+        Console.WriteLine("Hola " + nombre + ", me llamo " + this.nombre);
+    }
+}
---
+++ src/ClasesDictadas/Clase02-20260807/Ejercicio02/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+
+Vehiculo vehiculo = new Vehiculo("Ford", "AA000AA", "Fiesta");
+vehiculo.Info();
+//vehiculo.pantete = "AA000AA";
+
+
+public class Vehiculo
+{
+    public string marca;
+    private string patente;
+    public string modelo;
+
+    public Vehiculo(string marca, string p_patente, string modelo)
+    {
+        this.marca=marca;
+        this.patente=p_patente;
+        this.modelo=modelo;
+    }
+    
+    public void Info ()
+    {
+        Console.WriteLine("Marca: " + this.marca + " Modelo: " + this.modelo + " Patente: " + patente); 
+    }
+
+}
