cc7d03e4089600e6bf879bac47212442447c23bb
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-08-28 22:00:18 -0300

    Subimos archivos

--- src/AutoEvaluaciones/02-AE/Program.cs
+++ src/AutoEvaluaciones/02-AE/Program.cs
@@ -99,7 +99,7 @@ class DerivedClass : BaseClass
 */
 
 // Pregunta 14
-
+/*
 Car myCar = new Car();
 myCar.Honk(); // Llamada al método de la clase base
 myCar.TurnEngineOn(); // Llamada al método de la clase derivada
@@ -119,5 +119,124 @@ class Car : Vehicle
     {
         Console.WriteLine("Encendiendo el motor del coche está encendido.");
         return true;
+    }
+}
+*/
+/*
+public class Program
+{
+    static void Main()
+    {
+        C cInstance = new C();
+        cInstance.Method(); // Llamada al método de la clase base
+    }
+}
+
+class A
+{
+    public virtual void Method()
+    {
+        Console.WriteLine("Método de la clase A");
+    }
+}
+
+class B : A
+{
+    public override void Method()
+    {
+        Console.WriteLine("Método de la clase B");
+    }
+}
+
+class C : B
+{
+    public override void Method()
+    {
+        base.Method();
+        Console.WriteLine("Método de la clase C");
+    }
+}
+*/
+
+/*
+
+class Person
+{
+    protected virtual string GetFullName()
+    {
+        return "John Doe";
+    }
+
+    public virtual void SayHi()
+    {
+        Console.WriteLine($"Hola!");
+    }
+}
+
+class Employee : Person
+{
+    public double GetFile()
+    {
+        return 12345;
+    }
+
+    public override void SayHi()
+    {
+        base.SayHi(); // Lalama al metodo de la clase base
+        Console.WriteLine($"Mi legajo es {this.GetFile()}");
+    }
+}
+*/
+
+/*
+Shape MyShape = new Shape();
+abstract class Shape
+{
+    public abstract double CalculateArea();
+    public virtual string GetShapeName()
+    {
+        return "Forma Genérica";
+    }
+}
+
+class Circle : Shape
+{
+    private double radius;
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+*/
+
+Printer generalPrinter = new HigResolutionPrinter(); // Upcasting
+generalPrinter.PrintDocument("reporte Anual");
+
+generalPrinter.Calibrate();
+// Error: No se puede acceder al método de la clase derivada a través de la referencia de la clase base
+
+class Printer
+{
+    public virtual void PrintDocument(string doc)
+    {
+        Console.WriteLine($"Imprimiendo: {doc}");
+    }
+}
+
+class HigResolutionPrinter : Printer
+{
+    public override void PrintDocument(string doc)
+    {
+        Console.WriteLine($"Imprimiendo en alta resolución: {doc}");
+    }
+
+    public void Calibrate()
+    {
+        Console.WriteLine("Calibrando...");
     }
 }
