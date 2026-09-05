d98bbfac3cd43c56fcd8f5af0a39f10eb2731cc9
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-09-05 17:05:27 -0300

    Tutoría presencial

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
 Console.WriteLine("Hello, World!");
+
+Clase 05-09-2026
+
+Las interfaces son habilidades. <i>costeable, calculable
+
+
+*/
