e02431900740932e5a0d081c61dd36f2957b138f
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-09-08 20:51:23 -0300

    Subimos lo de hoy!

--- test/ConsoleApp/Program.cs
+++ test/ConsoleApp/Program.cs
@@ -1,9 +1,25 @@
 ﻿/*
 Console.WriteLine("Hello, World!");
-
 Clase 05-09-2026
-
 Las interfaces son habilidades. <i>costeable, calculable
+*/
 
+/* Clase 11
 
 */
+// 1. Primero el código ejecutable
+DiaSemana hoy = DiaSemana.Sabado;
+
+switch (hoy)
+{
+    case DiaSemana.Sabado:
+    case DiaSemana.Domingo:
+        Console.WriteLine("Fin de semana");
+        break;
+    default:
+        Console.WriteLine("Día laboral");
+        break;
+}
+
+// 2. Al final las declaraciones de tipos
+enum DiaSemana { Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo }
