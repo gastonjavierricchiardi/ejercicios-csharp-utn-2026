889c4808a9a7d55f0297f45eff885e9ebbdde40f
Gaston Javier Ricchiardi <192486168+gastonjavierricchiardi@users.noreply.github.com>
2026-09-22 12:41:06 -0300

    Ajustes secundarios

---
+++ Program(base).cs
@@ -0,0 +1,29 @@
+﻿// src\Muebleria-preParcial\Program.cs
+// gastonj@hotmail.com
+
+/*
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear objetos y probar el ejercicio
+    }
+}
+
+public class Clase
+{
+    // 1. CAMPOS / ATRIBUTOS
+    // Estado interno del objeto.
+    // Normalmente private.
+
+    // 2. CONSTRUCTOR
+    // Recibe los datos necesarios al crear el objeto.
+
+    // 3. PROPIEDADES / GETTERS Y SETTERS
+    // Formas de exponer o modificar el estado.
+
+    // 4. MÉTODOS
+    // Comportamiento del objeto.
+}
+
+*/
